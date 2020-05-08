5c9b8a85c1f14c7d261c0536cd371da503aac98b
Diego Bernardes <diego.bernardes@gonitro.com>
2020-05-08 12:09:13 +0100

    Bootstrap the action

---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+
+# Exit with a error status in case of command error.
+set -e
+
+# Read the parameters.
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --version) version="$2"; shift ;;
+        --config) config="$2"; shift ;;
+        --path) path="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Download the release.
+curl -L -o markdown-link-check.tar.gz https://github.com/Nitro/markdown-link-check/releases/download/${version}/markdown-link-check_${version}_linux_amd64.tar.gz
+
+# Uncompress.
+tar -xvf markdown-link-check.tar.gz
+
+# Move the binary to '/usr/bin'.
+mv markdown-link-check_${version}_linux_amd64/markdown-link-check /usr/bin/
+
+# Remove the temporary files.
+rm -Rf markdown-link-check*
+
+# Excute the command.
+markdown-link-check --config $config $path
--- readme.md
+++ readme.md
@@ -1,2 +1,22 @@
 # Markdown Link Check Action
-GitHub Actions for the [markdown-link-check](https://github.com/Nitro/markdown-link-check) application.
+This action uses [markdown-link-check](https://github.com/Nitro/markdown-link-check) to check for broken links at markdown files.
+
+## Inputs
+### `version`
+Not required. Default value `v0.1.0`.
+The version correspond to these [releases](https://github.com/Nitro/markdown-link-check/releases).
+
+### `config`
+Not required. Default value `markdown-link-check.yml`.
+
+### `path`
+Not required. Default value `.`.
+
+## Example usage
+```yml
+uses: Nitro/markdown-link-check-action@v1
+with:
+  version: v0.1.0
+  config: markdown-link-check.yml
+  path: .
+```
